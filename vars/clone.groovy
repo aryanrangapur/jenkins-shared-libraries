ab00bcebb0878e842dcfda3395e971784059751c
Aryan Rangapur <154872205+aryanrangapur@users.noreply.github.com>
2025-02-26 04:46:07 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,5 +1,5 @@
 def call(String url, String branch){
    echo "Cloning from GitHub..."
-    git url: ${url}, branch:${branch}
+    git url: "${url}", branch:"${branch}"
     echo "Cloned Succesfully!"
 }
